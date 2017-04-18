331b45b781528d68337e615b4e967b109edc332a
Ljf <675410221@qq.com>
2017-04-18 13:58:26 +0800

    original branch2

--- GitTest/AppDelegate.m
+++ GitTest/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    NSLog(@"Branch2");
+
     return YES;
 }
 
